148078a3aa5d819fb9c817b1a26a8c85260f33eb
Camilo Suaza <camilo.suaza12@gmail.com>
2019-10-12 20:36:49 -0500

    Adding markers to the map by a click without localStorage

---
+++ 12-GoogleMaps/maps/src/app/classes/marker.class.ts
@@ -0,0 +1,15 @@
+export class Marker {
+
+    public lat: number;
+    public lng: number;
+
+    public tittle = 'Sin título';
+    public description = 'Sin descripción';
+
+
+    constructor(lat: number, lng: number) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+}
--- 12-GoogleMaps/maps/src/app/components/map/map.component.ts
+++ 12-GoogleMaps/maps/src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marker } from '../../classes/marker.class';
 
 @Component({
   selector: 'app-map',
@@ -7,12 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  markers: Marker[] = [];
+
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor() { }
+  constructor() {
+
+    const NEW_MARKER = new Marker(51.678418, 7.809007);
+    this.markers.push(NEW_MARKER);
+  }
 
   ngOnInit() {
+  }
+
+  addMarker(event) {
+
+    const coords: {lat: number, lng: number} = event.coords;
+
+    const NEW_MARKER = new Marker(coords.lat, coords.lng);
+    this.markers.push(NEW_MARKER);
   }
 
 }
